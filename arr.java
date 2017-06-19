6020cc231ceac5a7a167d99b5b70a8768923a94a
maninagul <kongumani1996@gmail.com>
2017-06-19 14:52:03 +0530

    Add files via upload

---
+++ arr.java
@@ -0,0 +1,18 @@
+package array;
+
+public class arr {
+
+	public static void main(String[] args) {
+		int a[]={1,2,3,4,5,6,7};
+		a[0]=1;
+		a[1]=2;
+		a[2]=3;
+		a[3]=4;
+		a[4]=5;
+		a[5]=6;
+		a[6]=7;
+		System.out.println(a[6]+","+a[0]+","+a[5]+","+a[1]+","+a[4]+","+a[2]+","+a[3]);
+
+	}
+
+}
