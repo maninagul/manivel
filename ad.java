18abcddb1c55bcff1dc5e721bb2035208e9e025d
maninagul <kongumani1996@gmail.com>
2017-06-19 15:15:31 +0530

    Add files via upload

---
+++ ad.java
@@ -0,0 +1,16 @@
+package add;
+
+public class ad {
+
+	public static void main(String[] args) {
+		int a,b,c,i;
+		a=50;
+		b=10;
+	   c=a;
+		for(i=1;i<=b;i++){
+			c--;
+		}System.out.println(c);
+
+	}
+
+}
