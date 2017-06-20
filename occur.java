88d90f7b32a2fa2648c61f6ae7e6d4d98fcb8c6f
maninagul <kongumani1996@gmail.com>
2017-06-20 17:35:35 +0530

    Add files via upload

---
+++ occur.java
@@ -0,0 +1,13 @@
+package occurence;
+
+public class occur {
+
+	public static void main(String[] args){
+		String s1="welcome";
+		String s2=new StringBuffer(s1).reverse().toString();
+		System.out.println(s2);
+	}
+
+	}
+
+
