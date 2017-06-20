1e1a81d0b5198676da7a3ba1ae0977e61178d92b
maninagul <kongumani1996@gmail.com>
2017-06-20 17:51:38 +0530

    Add files via upload

---
+++ len.java
@@ -0,0 +1,12 @@
+package length;
+
+public class len {
+
+	public static void main(String[] args) {
+		String s1="welcome";
+		System.out.println(s1.substring(3,7));
+		System.out.println(s1.length());
+
+	}
+
+}
