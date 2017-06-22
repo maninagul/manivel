f71872be51f95baa4a1ea7ae3ad01540e58db299
maninagul <kongumani1996@gmail.com>
2017-06-22 15:19:46 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,17 @@
+
+public class power {
+
+	public static void main(String[] args) {
+    int a=17;  
+    if(((~a) & 1) == 1)  
+    {  
+        System.out.println("power of two");  
+    }  
+    else  
+    {
+        System.out.println("NOT A power of two"); 
+
+	}
+
+}
+}
