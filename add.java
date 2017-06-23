507f66d1ef49037f677e4830619d06063a0a3bdc
maninagul <kongumani1996@gmail.com>
2017-06-23 12:52:27 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,14 @@
+package sum;
+
+public class add {
+
+	public static void main(String[] args) {
+  int input=4;
+  int sum=0;
+  for(int i=1;i<=4;i++){
+	  sum=sum+i;
+ }
+   System.out.println(+sum);
+	}
+
+}
