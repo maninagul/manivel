6da59fd944a8144e0d907d12cba13353a0af94e7
maninagul <kongumani1996@gmail.com>
2017-06-23 14:42:52 +0530

    Add files via upload

---
+++ dog.java
@@ -0,0 +1,33 @@
+package dogbarks;
+
+public class dog {
+
+	public static void main(String[] args) {
+		String s1 ="wake up with fear";
+		String s2="cries";
+		String s3="smile";
+		switch(s1){
+		case "wake up with fear":System.out.println("dog barks thrice");break;
+		case "cries":System.out.println("dog barks twice");break;
+		case "smile":System.out.println("dog barks once");break;
+		default:System.out.println("invalid");
+		}
+		switch(s2){
+		case "wake up with fear":System.out.println("dog barks thrice");break;
+		case "cries":System.out.println("dog barks twice");break;
+		case "smile":System.out.println("dog barks once");break;
+		default:System.out.println("invalid");
+
+	}
+		switch(s3){
+		case "wake up with fear":System.out.println("dog barks thrice");break;
+		case "cries":System.out.println("dog barks twice");break;
+		case "smile":System.out.println("dog barks once");break;
+
+		
+		default:System.out.println("invalid");
+
+		}
+
+}
+}
