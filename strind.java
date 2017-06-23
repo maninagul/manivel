c1a4e19eac17a659e7712251a01c277fe6e4f5cc
maninagul <kongumani1996@gmail.com>
2017-06-23 15:39:29 +0530

    Add files via upload

---
+++ strind.java
@@ -0,0 +1,12 @@
+package indexstr;
+
+public class strind {
+
+	public static void main(String[] args) {
+		String s="racercar";
+		String s1=s.substring(0,4);
+		String s2=s1+s.substring(5,8);
+    System.out.println(s2);
+	}
+
+}
