2fb362f345682e19ad57c8199bcab7f75df10cb1
maninagul <kongumani1996@gmail.com>
2017-06-25 10:45:15 +0530

    Add files via upload

---
+++ strprefix.java
@@ -0,0 +1,27 @@
+package string;
+
+public class strprefix {
+
+	public static void main(String[] args) {
+		//String s4=String s;String s1,String s2,String s3;
+		String s= "geeksforgeeks";
+		String s1="geeks";
+		String s2="geezer";
+		String s3="geek";
+		
+		if(s.startsWith("gee")){
+			System.out.println("gee");
+		}
+		 if(s1.startsWith("gee")){
+		System.out.println("gee");
+		}
+		 if(s2.startsWith("gee")){
+		System.out.println("gee");
+		}
+		 if(s3.startsWith("gee")){
+		System.out.println("gee");
+
+}
+
+	}
+}
