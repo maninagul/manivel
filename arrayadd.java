ab1a3d11a414f51c606aebb21fd0961aa46d3860
maninagul <kongumani1996@gmail.com>
2017-06-25 11:29:32 +0530

    Add files via upload

---
+++ arrayadd.java
@@ -0,0 +1,18 @@
+package array;
+
+public class arrayadd {
+
+	public static void main(String[] args) {
+		int arr[]={2,1,6,5};
+		int target=9;
+		int ans;
+		arr[0]=2;
+		arr[1]=1;
+		arr[2]=6;
+		arr[3]=5;
+		ans=arr[0]+arr[1]+arr[2];
+		System.out.println(ans);
+
+	}
+
+}
