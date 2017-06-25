151b8b19f9f12655338c15d8b48d75608467cab1
maninagul <kongumani1996@gmail.com>
2017-06-25 12:00:00 +0530

    Add files via upload

---
+++ div.java
@@ -0,0 +1,18 @@
+package divide;
+
+public class div {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=2;
+		int c=0;
+		while(a>=b)
+		{
+			a=a-b;
+			c++;
+		}
+System.out.println("quotient"+c);
+System.out.println("remainder"+a);
+	}
+
+}
