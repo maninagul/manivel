1da9dd44aae9cf466edbf82a8b3c9923f538bfdc
maninagul <kongumani1996@gmail.com>
2017-07-04 10:23:54 +0530

    Add files via upload

---
+++ multiplystr.java
@@ -0,0 +1,16 @@
+package stringpgms;
+
+public class multiplystr {
+
+	public static void main(String[] args) {
+		String s1="4";
+		String s2="5";
+		int a=Integer.parseInt(s1.trim());
+		int b=Integer.parseInt(s2.trim());
+		int c=0;
+		if(a>0&&b>0){
+			c=a*b;
+		}System.out.println(c);
+	}
+
+}
