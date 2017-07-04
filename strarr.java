82622391000fe0b2f87a91452d94281ac2fac5eb
maninagul <kongumani1996@gmail.com>
2017-07-04 11:55:15 +0530

    Add files via upload

---
+++ strarr.java
@@ -0,0 +1,17 @@
+package string;
+
+public class strarr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     String s1="arun#12#12#10";
+     String s2="deepak#13#13#10";
+     if(s1.length()>s2.length()){
+    	 System.out.println(s1);
+    	 }
+     else{
+    	 System.out.println(s2);
+     }
+	}
+
+}
