a114795798c8fd6314758d514d5224f66052283d
maninagul <kongumani1996@gmail.com>
2017-07-10 16:35:06 +0530

    Add files via upload

---
+++ Firstoccur.java
@@ -0,0 +1,15 @@
+package string;
+
+public class Firstoccur {
+
+	public static void main(String[] args) {
+		String a[]={"AF","D","EF","G"};
+		for(int c=0;c<a.length;c++) {
+		if(a[c].length()==1) {
+			System.out.println(a[c]);
+		}
+
+	}
+
+	}
+}
