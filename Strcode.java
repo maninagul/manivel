15dec25b950be5d7dfc91547c4425c857b3f91e0
maninagul <kongumani1996@gmail.com>
2017-07-10 17:09:53 +0530

    Add files via upload

---
+++ Strcode.java
@@ -0,0 +1,11 @@
+package string;
+
+public class Strcode {
+
+	public static void main(String[] args) {
+		String s="a1b10";
+		System.out.println("a"+"b"+"b"+"b"+"b"+"b"+"b"+"b"+"b"+"b"+"b");
+
+	}
+
+}
