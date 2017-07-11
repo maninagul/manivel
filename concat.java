aa459aa63dff45da9e358baaf522168a61fe8966
maninagul <kongumani1996@gmail.com>
2017-07-11 14:32:35 +0530

    Add files via upload

---
+++ concat.java
@@ -0,0 +1,12 @@
+package string;
+import java.util.Scanner;
+public class concat {
+public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		String s1=s.next();
+		String s2=s.next();
+		System.out.println(s1+s2);
+
+	}
+
+}
