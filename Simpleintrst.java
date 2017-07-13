62cab4efffb5a2f5f94651b706c8ba2dce1cc92f
maninagul <kongumani1996@gmail.com>
2017-07-13 09:43:50 +0530

    Add files via upload

---
+++ Simpleintrst.java
@@ -0,0 +1,15 @@
+package string;
+import java.util.Scanner;
+public class Simpleintrst {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int p=s.nextInt();
+		int n=s.nextInt();
+		int r=s.nextInt();
+		int x=((p*n*r)/100);
+		System.out.println(x);
+
+	}
+
+}
