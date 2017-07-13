15aba8e4b7256c0a3ff9d58ba10f2acd23248237
maninagul <kongumani1996@gmail.com>
2017-07-13 11:28:29 +0530

    Add files via upload

---
+++ Strcrunch.java
@@ -0,0 +1,10 @@
+package string;
+
+public class Strcrunch {
+public static void main(String[] args) {
+	String s="this is a beautiful place to get tech questions";
+	String s1=s.replaceAll("[ise]","");
+System.out.println(s1);
+	}
+
+}
