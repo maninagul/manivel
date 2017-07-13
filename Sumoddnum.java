6221e8b2e3d5aa9e91642b33a5e591a5c02a2f45
maninagul <kongumani1996@gmail.com>
2017-07-13 09:51:41 +0530

    Add files via upload

---
+++ Sumoddnum.java
@@ -0,0 +1,14 @@
+package string;
+
+public class Sumoddnum {
+
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=0;i<20;i++){
+		if(i%2!=0){
+		sum=sum+i;}}
+		System.out.println(+sum);
+		}
+	}
+
+
