703b5249b6d04b40416a9a9456097c6e697597a7
maninagul <kongumani1996@gmail.com>
2017-07-14 14:34:33 +0530

    Add files via upload

---
+++ Strarr.java
@@ -0,0 +1,10 @@
+package guvi;
+import java.util.Scanner;
+public class Strarr {
+public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+String str[]=s.nextLine().split(" ");
+System.out.println(str[1]+"("+str[1].length()+")");
+	}
+
+}
