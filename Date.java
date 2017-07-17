fe008acce2b51b49586fc4a715d7c2a5a6e49da3
maninagul <kongumani1996@gmail.com>
2017-07-17 10:41:49 +0530

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,64 @@
+package guvi;
+import java.util.Scanner;
+public class Date {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter the date");
+	int a=s.nextInt();
+	System.out.println("Enter the month");
+	int b=s.nextInt();
+	System.out.println("Enter the year");
+	int c=s.nextInt();
+	 if(b==1)
+     {
+    	 System.out.println("january");
+     }
+     if(b==2)
+     {
+    	 System.out.println("febrauary");
+     }
+     if(b==3)
+     {
+    	 System.out.println("march");
+     }
+     if(b==4)
+     {
+    	 System.out.println("april");
+     }
+     if(b==5)
+     {
+    	 System.out.println("may");
+     }
+     if(b==6)
+     {
+    	 System.out.println("june");
+     }
+     if(b==7)
+     {
+    	 System.out.println("july");
+     }
+     if(b==8)
+     {
+    	 System.out.println("August");
+     }
+     if(b==9)
+     {
+    	 System.out.println("September");
+     }
+     if(b==10)
+     {
+    	 System.out.println("October");
+     }
+     if(b==11)
+     {
+    	 System.out.println("November");
+     }
+     if(b==12)
+     {
+    	 System.out.println("December");
+     }
+	 
+
+	}
+
+}
