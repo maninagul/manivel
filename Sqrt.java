8e11311721cd69aa5a85696124adb01d89be2459
maninagul <kongumani1996@gmail.com>
2017-07-17 10:28:10 +0530

    Add files via upload

---
+++ Sqrt.java
@@ -0,0 +1,15 @@
+package guvi;
+import java.util.Scanner;
+public class Sqrt {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+    int a1=10;
+	int a2=20;
+	int b1=3;
+	int b2=54;
+	int v;
+	v=(int) Math.sqrt(a1*a2+b1*b2);
+	System.out.println(v);
+	}
+
+}
