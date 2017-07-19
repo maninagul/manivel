6904d6923adc89c62366c171c08b772256809630
maninagul <kongumani1996@gmail.com>
2017-07-19 10:16:49 +0530

    Add files via upload

---
+++ Returnmul.java
@@ -0,0 +1,14 @@
+package guvi;
+import java.util.Scanner;
+public class Returnmul {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	String str=s.next();
+	String str1=s.next();
+    int n=Integer.parseInt(str);
+	int n1=Integer.parseInt(str1);
+  System.out.println(n*n1);
+
+	}
+
+}
