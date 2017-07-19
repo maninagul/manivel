192fd35850091cb542b49b5328fff9c2b5300c3f
maninagul <kongumani1996@gmail.com>
2017-07-19 10:12:50 +0530

    Add files via upload

---
+++ Multiplystr.java
@@ -0,0 +1,15 @@
+package guvi;
+import java.util.Scanner;
+public class Multiplystr {
+public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String str=s.next();
+		String str1=s.next();
+        int n=Integer.parseInt(str);
+		int n1=Integer.parseInt(str1);
+      //int m=n*n1;
+		System.out.println(n*n1);
+
+	}
+
+}
