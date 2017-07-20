e50872f661b404767659e7023af50aa59038ccb1
maninagul <kongumani1996@gmail.com>
2017-07-20 18:01:36 +0530

    Add files via upload

---
+++ Numeric.java
@@ -0,0 +1,22 @@
+package guvi;
+import java.util.Scanner;
+public class Numeric {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String a=s.nextLine();
+		if(a.contains("0")||a.contains("1")||a.contains("2")||a.contains("3")||a.contains("4")||a.contains("5")||a.contains("6")||a.contains("7")||a.contains("8")||a.contains("9")){
+			System.out.println("true");
+		}
+		else{
+			System.out.println("false");
+
+		}
+		
+		
+	
+	}
+
+
+	}
+
