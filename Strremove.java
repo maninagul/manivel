c1fcd9d15cc749d5a1a71ecd1e309eed6dc3f2e0
maninagul <kongumani1996@gmail.com>
2017-07-24 14:43:33 +0530

    Add files via upload

---
+++ Strremove.java
@@ -0,0 +1,13 @@
+package guvi;
+
+public class Strremove {
+
+	public static void main(String[] args) {
+		String mask="in";
+		String input="beingibbbing";
+		String sa=input.replaceAll(mask,"");
+		System.out.println(sa);
+
+	}
+
+}
