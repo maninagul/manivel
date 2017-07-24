78b7f5e2f0e82996d8ffb856ba930e3356d4dfdc
maninagul <kongumani1996@gmail.com>
2017-07-24 15:15:07 +0530

    Add files via upload

---
+++ Impstrlen.java
@@ -0,0 +1,13 @@
+package guvi;
+import java.util.Scanner;
+public class Impstrlen {
+
+	public static void main(String[] args) {
+   Scanner s=new Scanner(System.in);
+   String s1=s.nextLine();
+   //int a=s.nextInt();
+    int a=s1.length();
+    System.out.println(a);
+	}
+
+}
