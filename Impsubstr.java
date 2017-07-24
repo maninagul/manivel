2ead4a5d53e1680837fb3ca5f58a2876fd2d5b7a
maninagul <kongumani1996@gmail.com>
2017-07-24 15:08:10 +0530

    Add files via upload

---
+++ Impsubstr.java
@@ -0,0 +1,17 @@
+package guvi;
+import java.util.Scanner;
+public class Impsubstr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+	       String str=s.nextLine();
+	       String str1=s.nextLine();
+	       if(str1.contains(str)){
+	       System.out.println("yes");
+	       }else{
+	       System.out.println("no");
+	       }
+	}
+
+}
