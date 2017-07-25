7e3c26d4c444f93460e2d593fca8be9f87b0a41e
maninagul <kongumani1996@gmail.com>
2017-07-25 10:05:29 +0530

    Add files via upload

---
+++ Recrev.java
@@ -0,0 +1,13 @@
+package guvi;
+import java.util.Scanner;
+public class Recrev {
+
+	public static void main(String[] args) {
+	  Scanner s=new Scanner(System.in);
+	  String str[]= {"i"," ","love"," ","india"," "};
+	  
+	  System.out.println(str[4]+str[3]+str[2]+str[1]+str[0]);
+
+	}
+
+}
