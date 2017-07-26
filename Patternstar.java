9f3ca518dad03d126ff5990b228120869a00a4da
maninagul <kongumani1996@gmail.com>
2017-07-26 14:08:55 +0530

    Add files via upload

---
+++ Patternstar.java
@@ -0,0 +1,16 @@
+package guvi;
+
+public class Patternstar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j;
+		for(i=1;i<=6;i++) {
+			for(j=1;j<i;j++) {
+				System.out.print('*');
+			}
+			System.out.println();
+	}
+
+	}
+}
