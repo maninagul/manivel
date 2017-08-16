5406a16e16898935dec4468deabdacc6edc603de
maninagul <kongumani1996@gmail.com>
2017-08-16 10:33:05 +0530

    Add files via upload

---
+++ Mn.java
@@ -0,0 +1,17 @@
+package guvi;
+
+public class Mn {
+
+	public static void main(String[] args) {
+		String s1="1234xyz";
+		String s2="nmbvsdvtf123";
+		if(s1.contains("123")&&(s2.contains("123"))) {
+			System.out.println(true);
+		}
+		else {
+			System.out.println(false);
+		}
+
+	}
+
+}
